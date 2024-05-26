69bf7b5419c7285467c2ad2a89c05ab7372d8163
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-05-26 14:27:14 -0700

    add tune configs

--- projects/train/train/tune/search_space.py
+++ projects/train/train/tune/search_space.py
@@ -1,9 +1,11 @@
 from ray import tune
 
 space = {
-    "model.learning_rate": tune.loguniform(1e-4, 1e-1),
+    "model.init_args.arch.init_args.kernel_size": tune.choice([3, 7, 15]),
+    "model.init_args.arch.init_args.layers": tune.choice(
+        [[2, 3, 4], [3, 4, 6, 3], [3, 4, 6, 5, 4, 4, 3]]
+    ),
+    "model.learning_rate": tune.loguniform(1e-4, 1e-2),
     "model.pct_lr_ramp": tune.uniform(0.05, 0.7),
-    "data.swap_prob": tune.uniform(0.0, 0.15),
-    "data.mute_prob": tune.uniform(0.0, 0.15),
     "data.waveform_prob": tune.uniform(0.2, 0.6),
 }
